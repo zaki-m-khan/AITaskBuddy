1600b37c63bcd6b31b508a90a7bc346f0565c3b0
zaki-m-khan <zakikhan.contact@gmail.com>
2025-03-20 09:33:22 -0400

    hiding api keys

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,15 +1,16 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 // Replace with your own Firebase project config
 const firebaseConfig = {
-  apiKey: "AIzaSyDUllPnXcPqEu4Du4wwKFDDoFcEdDXs5p8",
-  authDomain: "aitaskbuddy-8908c.firebaseapp.com",
-  projectId: "aitaskbuddy-8908c",
-  storageBucket: "aitaskbuddy-8908c.firebasestorage.app",
-  messagingSenderId: "765422405339",
-  appId: "1:765422405339:web:34d2f957f420fe1809eefe"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
@@ -17,4 +18,6 @@ const app = initializeApp(firebaseConfig);
 
 // Initialize Firebase Authentication and get a reference to the service
 export const auth = getAuth(app);
+export const db = getFirestore(app);
+
 export default app; 
